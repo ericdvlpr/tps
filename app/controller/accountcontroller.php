7d494532984f86ae1fed90273928cc3524b8069a
Eric Jaucian <gitemail@shoperones.com>
2020-08-12 09:22:03 +0800

    dashboard changes and libraries update

--- app/controller/accountcontroller.php
+++ app/controller/accountcontroller.php
@@ -6,8 +6,21 @@ class AccountController extends Account
 {
     public function can_login($fields){
        $login = $this->login_account($fields);
-       if($login){
-            header('location:./index.php');
+       if(!empty($login)){
+          $_SESSION['username'] = $login['username'];
+          $_SESSION['id'] = $login['username'];
+          $_SESSION['access'] = $login['access'];
+          switch($login['access']){
+               case 1:
+                    header('location:/admin');
+               break;
+               case 2:
+                    header('location:index.php');
+               break;
+               default:
+                    header('location:login.php');
+          }
+          //   header('location:./index.php');
        }else{
             header('location:./index.php');
        }
--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,7 @@
 <!-- Developed by: ERIC PAUL JAUCIAN -->
  <?php
-
- include 'app/database.php';
- $object = new Database();
+ include '../../app/controller/dashboardcontroller.php';
+ $dashboard = new DashboardController();
  // if(!isset($_SESSION["access"])){
  //   header('location:../login.php');
  // }
@@ -10,22 +9,19 @@
  <html>
       <head>
    		<title>Transaction Processing System</title>
-      <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
       <!-- Font Awesome -->
-      <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
       <!-- Ionicons -->
-      <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
-      <!-- jvectormap -->
-      <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
+      <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+      
+      <!-- <link rel="stylesheet" href="bower_components/jvectormap/jquery-jvectormap.css">
       <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
-      <!-- daterange picker -->
+     
       <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
-      <!-- bootstrap datepicker -->
-      <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
-      <!-- Theme style -->
+      
+      <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css"> -->
       <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-      <!-- AdminLTE Skins. Choose a skin from the css/skins
-           folder instead of downloading all of them to reduce the load. -->
       <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
 
       </head>
@@ -34,8 +30,8 @@
 <body class="hold-transition skin-blue sidebar-mini">
 <!-- Site wrapper -->
 <div class="wrapper">
-        <?php include 'includes/nav.php'; ?>
-        <?php include 'includes/sidemenu.php'; ?>
+        <?php include '../../includes/nav.php'; ?>
+        <?php include '../../includes/sidemenu.php'; ?>
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
           <!-- Content Header (Page header) -->
